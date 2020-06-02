6e39102fb30f793a61f054e2616d942cf260ff53
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-06-01 21:59:56 -0400

    eod commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //import express
 const express = require('express');
-const shortid = require('shortid');
+// const shortid = require('shortid');
 
 //create a server
 const server = express();
@@ -9,7 +9,7 @@ server.use(express.json());
 //data points
 let users = [
     {
-        id: shortid.generate(),
+        id: 1,
         name: "Bobby Gondola",
         bio: "my name is bobby and i love to code"
 
@@ -22,21 +22,63 @@ server.get('/', (req, res) => {
     res.send('hello from the / endpoint in the server')
 })
 
+//get /users
 server.get('/users', (req, res) => {
-    res.status(200).json(users)
+    if ('/users' !== undefined) {
+        res.status(200).json(users)
+    } else {
+        res.status(500).json({errorMessage: "The users information could not be retrieved."})
+    }
 })
+//post /users
 server.post('/users', (req, res) => {
     const user = req.body;
+    if (user.name === undefined || user.bio === undefined) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    }
+    else if (user.name !== undefined || user.bio !== undefined) {
     users.push(user)
     res.status(201).json(users)
-})
-server.delete('/users/:id', (req, res) => {
-    const id = req.params.id;
-    users = users.filter(u => u.id !== Number(id))
-    res.status(200).json(users)
+    }
+    else{
+    res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
+    }
 })
 
+//get /users/:id
+server.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const singleUser = users.find(user => user.id === Number(id))
+    
+    if(singleUser){
+        res.status(200).json(singleUser)
+    } else {
+        res.status(404).json({ errorMessage: "The user information could not be retrieved." })
+    }
+});
+
+//delete /users/:id
+server.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedUsers = users.filter(u => u.id !== Number(id))
+    if (id === undefined) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    } else {
+        res.status(200).json(updatedUsers)
+    }
+})
+
+//put
+server.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    if (changes.name === undefined || changes.bio === undefined) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {
+        res.send()
+    }
+})
 
 //server to listen for incoming requests 
 const port = 8001;
-server.listen(port, () => console.log(`server is listening on ${port}, good luck and work hard`));
+server.listen(port, () => console.log(`hello bobby, i am now sentient.. also on port ${port}`));
